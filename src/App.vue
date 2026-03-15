c8dfbdd2e00fe39d9b4220876360e57a7dffd6c2
lunalobos <lunalobosmiguel@gmail.com>
2026-03-15 15:00:27 -0300

    Layout inicial

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
-<script setup></script>
+<script setup>
+import SideBar from "@/components/SideBar.vue";
+import NavBar from "@/components/NavBar.vue"
+</script>
 
 <template>
-  <h1 class="font-medium text-lg text-green-500">Hello word</h1>
- 
+  <div class="grid max-h-screen w-screen grid-cols-12 justify-stretch">
+    <SideBar />
+    <div class="col-span-10 grid h-full w-full grid-rows-24 justify-stretch">
+      <NavBar />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/icons/Archive.vue
@@ -0,0 +1,18 @@
+<template>
+  <svg
+    class="w-10 h-10 text-gray-800 dark:text-white"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+  >
+    <path
+      fill-rule="evenodd"
+      d="M20 10H4v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8ZM9 13v-1h6v1a1 1 0 0 1-1 1h-4a1 1 0 0 1-1-1Z"
+      clip-rule="evenodd"
+    />
+    <path d="M2 6a2 2 0 0 1 2-2h16a2 2 0 1 1 0 4H4a2 2 0 0 1-2-2Z" />
+  </svg>
+</template>
