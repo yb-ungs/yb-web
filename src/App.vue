ab08f69c1a8601a7379c7439f6bb201a3cb6c4fd
lunalobos <lunalobosmiguel@gmail.com>
2026-03-15 13:26:38 -0300

    tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="font-medium text-lg text-green-500">Hello word</h1>
+ 
 </template>
 
 <style scoped></style>
